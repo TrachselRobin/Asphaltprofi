cc349ba2fd363822c7d51e1538f40bcbf280b8af
trachselr <trachselr@bzz.ch>
2024-02-05 12:07:53 +0100

    UPDATED: Startseite Header

---
+++ Frontend/script/index.js
@@ -0,0 +1,43 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const CAROUSEL = document.getElementById('carousel');
+
+    const CAROUSEL_CHILDREN = CAROUSEL.children.length;
+
+    /*
+        Create a new keyframes animation for the carousel that depends on the number of children.
+        For two children, the animation should look like this:
+        @keyframes carousel {
+            0% { right: 0vw; }
+            45% { right: 0vw; }
+            50% { right: 100vw; }
+            95% { right: 100vw; }
+            100% { right: 0vw; }
+        }
+        For three children, the animation should look like this:
+        @keyframes carousel {
+            0% { right: 0vw; }
+            30% { right: 0vw; }
+            33.33% { right: 100vw; }
+            63.33% { right: 100vw; }
+            66.66% { right: 200vw; }
+            96.66% { right: 200vw; }
+            100% { right: 0vw; }
+        }
+    */
+    let keyframes = `@keyframes carousel {\n\t0% { transform: translateX(0vw); }`;
+    for (let i = 0; i < CAROUSEL_CHILDREN; i++) {
+        keyframes += `\n\t${(i+1) * 90 / CAROUSEL_CHILDREN + (10 / CAROUSEL_CHILDREN) * i}% { transform: translateX(-${(i) * 100}vw); }`;
+        if (i >= CAROUSEL_CHILDREN - 1) {
+            keyframes += `\n\t100% { transform: translateX(0vw); }`;
+        } else {
+            keyframes += `\n\t${(i+1) * 90 / CAROUSEL_CHILDREN + (10 / CAROUSEL_CHILDREN) * i + 10 / CAROUSEL_CHILDREN}% { transform: translateX(-${(i+1) * 100}vw); }`;
+        }
+    }
+    keyframes += `\n}`;
+
+    const STYLE = document.createElement('style');
+    STYLE.innerHTML = keyframes;
+    document.head.appendChild(STYLE);
+
+    CAROUSEL.style.animation = `carousel ${5 * CAROUSEL_CHILDREN}s infinite`;
+});
