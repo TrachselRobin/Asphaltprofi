9dcfc3ad5e13738bde45b5d54b3f876e84b0188f
trachselr <trachselr@bzz.ch>
2024-02-07 13:57:42 +0100

    UPDATED: Origin of standard css

--- Frontend/script/index.js
+++ Frontend/script/index.js
@@ -4,11 +4,7 @@ document.addEventListener('DOMContentLoaded', function () {
     window.addEventListener('scroll', function () {
         let currentScroll = window.pageYOffset || document.documentElement.scrollTop;
         if (currentScroll > 100) {
-            if (NAVBAR.classList.contains('light')) {
-                NAVBAR.style.backgroundColor = 'rgba(255, 255, 255, 0.9)';
-            } else {
-                NAVBAR.style.backgroundColor = 'rgba(0, 0, 0, 0.9)';
-            }
+            NAVBAR.style.backgroundColor = 'rgba(0, 0, 0, 0.9)';
         } else {
             NAVBAR.style.backgroundColor = 'rgba(0, 0, 0, 0)';
         }
@@ -20,6 +16,26 @@ document.addEventListener('DOMContentLoaded', function () {
             toggleMenu();
         }
     });
+
+    const CAROUSEL = document.getElementById('carousel');
+    const CAROUSEL_CHILDREN = CAROUSEL.children.length;
+    
+    let keyframes = `@keyframes carousel {\n\t0% { transform: translateX(0vw); }`;
+    for (let i = 0; i < CAROUSEL_CHILDREN; i++) {
+        keyframes += `\n\t${(i+1) * 90 / CAROUSEL_CHILDREN + (10 / CAROUSEL_CHILDREN) * i}% { transform: translateX(-${(i) * 100}vw); }`;
+        if (i >= CAROUSEL_CHILDREN - 1) {
+            keyframes += `\n\t100% { transform: translateX(0vw); }`;
+        } else {
+            keyframes += `\n\t${(i+1) * 90 / CAROUSEL_CHILDREN + (10 / CAROUSEL_CHILDREN) * i + 10 / CAROUSEL_CHILDREN}% { transform: translateX(-${(i+1) * 100}vw); }`;
+        }
+    }
+    keyframes += `\n}`;
+
+    const STYLE = document.createElement('style');
+    STYLE.innerHTML = keyframes;
+    document.head.appendChild(STYLE);
+
+    CAROUSEL.style.animation = `carousel ${5 * CAROUSEL_CHILDREN}s infinite`;
 });
 
 function toggleMenu() {
