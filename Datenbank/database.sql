317e2c901217ad61d6d98ec56558025376535c71
trachselr <trachselr@bzz.ch>
2024-02-09 14:47:04 +0100

    UPDATED: default values database

--- Datenbank/database.sql
+++ Datenbank/database.sql
@@ -52,7 +52,8 @@ CREATE TABLE address (
     `ID` INTEGER AUTO_INCREMENT, PRIMARY KEY (ID),
     `street` VARCHAR(40),
     `number` INTEGER,
-    `zip` INTEGER
+    `zip` INTEGER,
+    `city` VARCHAR(40)
 );
 
 CREATE TABLE chat (
@@ -114,12 +115,16 @@ INSERT INTO `tag` (name) VALUES ('Stock');
 INSERT INTO `tag` (name) VALUES ('Modified');
 INSERT INTO `tag` (name) VALUES ('Racing only');
 
-INSERT INTO `address` (street, number, zip) VALUES ('Musterstrasse', 1, 8000);
+INSERT INTO `address` (street, number, zip, city) VALUES ('Musterstrasse', 1, 8000, 'Zürich');
+INSERT INTO `address` (street, number, zip, city) VALUES ('Maxistrasse', 2, 8001, 'Zürich');
 
 INSERT INTO `vehicle` (brand, model, image, year, hp, ccm, tagID) VALUES ('BMW', 'M3', 'https://www.bmw.ch/content/dam/bmw/common/all-models/m-series/m3-sedan/2019/inspire/bmw-m-series-m3-sedan-inspire-01.jpg', 2019, 431, 2979, 1);
+INSERT INTO `vehicle` (brand, model, image, year, hp, ccm, tagID) VALUES ('Audi', 'RS6', 'https://www.audi.ch/content/dam/nemo/models/audi-rs6-avant/my-2020/1920x1080-gal-prop-tx/' , 2020, 600, 3993, 2);
 
 INSERT INTO `users` (prename, name, age, username, email, password, aboID, addressID) VALUES ('Max', 'Mustermann', 20, 'maxi', 'max@muster.ch', '1234', 1, 1);
+INSERT INTO `users` (prename, name, age, username, email, password, aboID, addressID) VALUES ('Hans', 'Muster', 25, 'hans', 'hans@muster.ch', '1234', 2, 2);
 
-INSERT INTO `user_vehicle` (userID, vehicleID) VALUES (1000000001, 1);
+INSERT INTO `user_vehicle` (userID, vehicleID) VALUES (1000000000, 1);
+INSERT INTO `user_vehicle` (userID, vehicleID) VALUES (1000000001, 2);
 
 SELECT * FROM `users`;
