941cc93059fe8d539d71038c54db559fe7ab31be
trachselr <trachselr@bzz.ch>
2024-02-12 19:43:03 +0100

    UPDATED: API

--- Backend/app.js
+++ Backend/app.js
@@ -9,8 +9,9 @@ const credentials = require('./credentials.json');
 APP.use(express.json());
 
 APP.get('/', async (req, res) => {
-    res.send("Helo");
-    log(req, res);
+    const TOKEN = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
+    res.send(TOKEN + " " + TOKEN.length);
+    log(req, res, "SUCCESS");
 });
 
 APP.get('/users', async (req, res) => {
@@ -19,7 +20,7 @@ APP.get('/users', async (req, res) => {
     const RESULT = await sqlQuery(SQL);
     res.send(RESULT);
 
-    log(req, res);
+    log(req, res, "SUCCESS");
 });
 
 APP.get('/user/:id', async (req, res) => {
@@ -29,6 +30,7 @@ APP.get('/user/:id', async (req, res) => {
     let result = await userIdExists(USERID);
     if (!result) {
         res.status(404).send("User not found");
+        log(req, res, "ERROR");
         return;
     }
 
@@ -36,18 +38,19 @@ APP.get('/user/:id', async (req, res) => {
     result = await sqlQuery(SQL);
     res.send(result);
 
-    log(req, res);
+    log(req, res, "SUCCESS");
 });
 
 APP.post('/user', async (req, res) => {
     const BODY = req.body;
-    const USER = {prename: BODY.prename, name: BODY.name, age: BODY.age, username: BODY.username, email: BODY.email, password: BODY.password, aboID: BODY.aboID};
+    const USER = {prename: BODY.prename, name: BODY.name, birthdate: BODY.birthdate, username: BODY.username, email: BODY.email, password: BODY.password, aboID: BODY.aboID};
     const ADDRESS = {street: BODY.street, city: BODY.city, zip: BODY.zip, number: BODY.number};
     
     // check if user email exists
     let result = await userEmailExists(USER.email);
     if (result) {
         res.status(409).send("Email already exists");
+        log(req, res, "ERROR");
         return;
     }
 
@@ -55,6 +58,7 @@ APP.post('/user', async (req, res) => {
     result = await userUsernameExists(USER.username);
     if (result) {
         res.status(409).send("Username already exists");
+        log(req, res, "ERROR");
         return;
     }
 
@@ -63,24 +67,25 @@ APP.post('/user', async (req, res) => {
         result = await sqlQuery(sql);
     }
     
-    sql = `INSERT INTO users (prename, name, age, username, email, password, aboID, addressID) VALUES ('${USER.prename}', '${USER.name}', ${USER.age}, '${USER.username}', '${USER.email}', '${USER.password}', ${USER.aboID}, (SELECT ID FROM address WHERE street = '${ADDRESS.street}' AND number = ${ADDRESS.number} AND zip = ${ADDRESS.zip} AND city = '${ADDRESS.city}'))`;
+    sql = `INSERT INTO users (prename, name, birthdate, username, email, password, aboID, addressID) VALUES ('${USER.prename}', '${USER.name}', '${USER.birthdate}', '${USER.username}', '${USER.email}', '${USER.password}', ${USER.aboID}, (SELECT ID FROM address WHERE street = '${ADDRESS.street}' AND number = ${ADDRESS.number} AND zip = ${ADDRESS.zip} AND city = '${ADDRESS.city}'))`;
     result = await sqlQuery(sql);
 
     res.send("User added");
     
-    log(req, res);
+    log(req, res, "SUCCESS");
 });
 
 APP.put('/user/:id', async (req, res) => {
     const BODY = req.body;
     const USEROLD = await getUser(req.params.id);
-    const USER = {prename: BODY.prename, name: BODY.name, age: BODY.age, username: BODY.username, email: BODY.email, password: BODY.password};
+    const USER = {prename: BODY.prename, name: BODY.name, birthdate: BODY.birthdate, username: BODY.username, email: BODY.email, password: BODY.password};
     const ADDRESS = {street: BODY.street, city: BODY.city, zip: BODY.zip, number: BODY.number, addressID: undefined};
     
     // check if user id exists
     let result = await userIdExists(req.params.id);
     if (!result) {
         res.status(404).send("User not found");
+        log(req, res, "ERROR");
         return;
     }
 
@@ -88,6 +93,7 @@ APP.put('/user/:id', async (req, res) => {
     result = await userEmailExists(USER.email, USEROLD.email);
     if (result) {
         res.status(409).send("Email already exists");
+        log(req, res, "ERROR");
         return;
     }
 
@@ -95,6 +101,7 @@ APP.put('/user/:id', async (req, res) => {
     result = await userUsernameExists(USER.username, USEROLD.username);
     if (result) {
         res.status(409).send("Username already exists");
+        log(req, res, "ERROR");
         return;
     }
 
@@ -112,12 +119,12 @@ APP.put('/user/:id', async (req, res) => {
         ADDRESS.addressID = result.insertId;
     }
 
-    sql = `UPDATE users SET prename = '${USER.prename}', name = '${USER.name}', age = ${USER.age}, username = '${USER.username}', email = '${USER.email}', password = '${USER.password}', addressID = ${ADDRESS.addressID} WHERE ID = ${req.params.id}`;
+    sql = `UPDATE users SET prename = '${USER.prename}', name = '${USER.name}', birthdate = '${USER.birthdate}', username = '${USER.username}', email = '${USER.email}', password = '${USER.password}', addressID = ${ADDRESS.addressID} WHERE ID = ${req.params.id}`;
     result = await sqlQuery(sql);
 
     res.send("User updated");
     
-    log(req, res);
+    log(req, res, "SUCCESS");
 });
 
 APP.delete('/user/:id', async (req, res) => {
@@ -128,6 +135,7 @@ APP.delete('/user/:id', async (req, res) => {
     let result = await userIdExists(USERID);
     if (!result) {
         res.status(404).send("User not found");
+        log(req, res, "ERROR");
         return;
     }
     
@@ -143,15 +151,88 @@ APP.delete('/user/:id', async (req, res) => {
         result = await sqlQuery(SQL);
     }
 
-    log(req, res);
+    log(req, res, "SUCCESS");
+});
+
+APP.post('/login', async (req, res) => {
+    const BODY = req.body;
+    const USER = {email: BODY.email, password: BODY.password};
+    
+    // check if user email exists
+    let result = await userEmailExists(USER.email);
+    if (!result) {
+        res.status(404).send("E-Mail not found");
+        log(req, res, "ERROR");
+        return;
+    }
+
+    // check if user password is correct
+    let sql = `SELECT * FROM users WHERE email = '${USER.email}' AND password = '${USER.password}'`;
+    result = await sqlQuery(sql);
+    if (result.length === 0) {
+        res.status(401).send("Wrong password");
+        log(req, res, "ERROR");
+        return;
+    }
+
+    // check if user is already logged in
+    sql = `SELECT token FROM users WHERE email = '${USER.email}' AND password = '${USER.password}' AND token IS NOT NULL`;
+    result = await sqlQuery(sql);
+    if (result.length !== 0) {
+        res.status(409).send("User already logged in");
+        log(req, res, "ERROR");
+        return;
+    }
+
+    const TOKEN = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
+    sql = `UPDATE users SET token = '${TOKEN}' WHERE email = '${USER.email}' AND password = '${USER.password}'`;
+    result = await sqlQuery(sql);
+
+    res.send(TOKEN);
+
+    log(req, res, "SUCCESS");
+});
+
+// verify: check if user is logged in
+APP.get('/verify', async (req, res) => {
+    const TOKEN = req.headers.authorization;
+    if (TOKEN === undefined) {
+        res.status(401).send("Not logged in");
+        log(req, res, "ERROR");
+        return;
+    }
+
+    const SQL = `SELECT * FROM users WHERE token = '${TOKEN}'`;
+    const RESULT = await sqlQuery(SQL);
+    if (RESULT.length === 0) {
+        res.status(401).send("Not logged in");
+        log(req, res, "ERROR");
+        return;
+    }
+    res.send("Logged in");
+
+    log(req, res, "SUCCESS");
 });
 
 APP.listen(PORT, () => {
     console.log('Listening on port 3000...')
 });
 
-function log(req, res) {
-    console.log(`${format(req.method, 6)} ${format(req.url, 25)} ${res.statusCode}`);
+function log(req, res, tag = "INFO") {
+    message = `${format(req.method, 6)} ${format(req.url, 25)} ${res.statusCode}`;
+    switch (tag) {
+        case "INFO":
+            break;
+        case "ERROR":
+            message = "\u001b[1;31m" + message + "\u001b[0m";
+            break;
+        case "SUCCESS":
+            message = "\u001b[1;32m" + message + "\u001b[0m";
+            break;
+        default:
+            break;
+    }
+    console.log(message);
 }
 
 function format(string, length) {
@@ -228,21 +309,7 @@ async function userUsernameExists(username, exception = undefined) {
 async function getUser(ID) {
     const SQL = `SELECT * FROM users WHERE ID = ${ID}`;
     const RESULT = await sqlQuery(SQL);
-    /*
-    RESULT = [
-        RowDataPacket {
-            ID: 1000000003,
-            prename: 'Robin',
-            name: 'Trachsel',
-            age: 19,
-            username: 'DoktorHodenlos',
-            email: 'ro.trachsel@vtxfre.ch',
-            password: '123',
-            aboID: 1,
-            addressID: 3
-        }
-    ]
-    */
-
+    
+    
     return RESULT[0];
 }
